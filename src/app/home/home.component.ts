bcb5fe670f049824f2cf56ba7d2af8f7314e9213
Javier Encina <encinajavier7@gmail.com>
2019-09-15 19:33:28 -0300

    End section 4

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  registerMode = false;
+  values: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getValues();
+  }
+
+  registerToggle(){
+    this.registerMode = true;
+  }
+
+  getValues() {
+    this.http.get('https://localhost:44309/api/values').subscribe((value) => {
+      this.values = value;
+      console.log(this.values);
+    }, error => {
+      console.log(error);
+    });
+
+  }
+
+  cancelRegisterMode(registerMode: boolean){
+    this.registerMode = registerMode;
+  }
+
+}
